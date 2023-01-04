979b08a802d9b27bd8e0e20a32469180e1ab7b9a
unknown <mahalingamnikhil@gmail.com>
2023-01-04 01:58:45 -0500

    Finished About date:  1/4

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 import * as T from 'three'; 
 import "./style.css"
+import gsap from "gsap"
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-
 
 //Scene
 const scene = new T.Scene;
 
+
 //Create our sphere
 const geometry = new T.SphereGeometry(3,64,64)
 const material = new T.MeshStandardMaterial({
-  color: "#8400ff",
+  color: "#0e88cf",
+  roughness: 0.4,
 })
 const mesh = new T.Mesh(geometry, material)
 scene.add(mesh)
@@ -23,6 +25,7 @@ const sizes = {
 //Light
 const light = new T.PointLight(0xffffff, 1, 100)
 light.position.set(0,10,10)
+light.intensity = 1.25
 scene.add(light)
 
 //Camera
@@ -65,3 +68,35 @@ const loop = () => {
   window.requestAnimationFrame(loop)
 }
 loop()
+
+//Initial timeline Animations
+const tl = gsap.timeline({defaults: { duration: 0.8}})
+tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z: 1, x: 1, y: 1})
+tl.fromTo('nav', {y: "-100%"}, {y: "0%"})
+tl.fromTo(".text-box p1", {opacity: 0}, {opacity: 1})
+tl.fromTo(".text-box h1", {opacity: 0}, {opacity: 1})
+tl.fromTo(".text-box h2", {opacity: 0}, {opacity: 1})
+tl.fromTo(".text-box p2", {opacity: 0}, {opacity: 1})
+
+//Mouse Animation (Color)
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener('mousemove', (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round( (e.pageX/sizes.width) * 255 ),
+      Math.round( (e.pageY/sizes.width) * 255 ),
+      150,
+    ]
+//Animate
+let newColor = new T.Color(`rgb(${rgb.join(",")})`)
+gsap.to(mesh.material.color, {
+  r: newColor.r, 
+  g: newColor.g,
+  b: newColor.b,
+    })
+  }
+})
